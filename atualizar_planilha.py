00f70564ff45d26c8183864f77a61eee7b8189e2
Jônatas Ribas <91293805+JonatasRibas@users.noreply.github.com>
2024-12-19 18:04:29 -0300

    commit

--- atualizar_planilha.py
+++ atualizar_planilha.py
@@ -25,7 +25,7 @@ def atualizar_planilha():
         print(f"Erro inesperado: {e}")
 
 # Agendar a tarefa
-schedule.every().day.at("17:59").do(atualizar_planilha)
+schedule.every().day.at("18:01").do(atualizar_planilha)
 
 # Loop infinito para manter o script rodando
 print("Automação iniciada. Aguardando horário programado...")
